a50f0dffa11b6caa4eaf50e426a304241d6b6434
Cassio Murilo <cmurilo.san@gmail.com>
2020-07-29 17:04:51 -0300

    Aula 02_B - Testando um SELECT com WHERE

--- test/br/com/caelum/pm73/dao/LeilaoDaoTest.java
+++ test/br/com/caelum/pm73/dao/LeilaoDaoTest.java
@@ -2,6 +2,8 @@ package br.com.caelum.pm73.dao;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.junit.After;
 import org.junit.Before;
@@ -38,7 +40,6 @@ public class LeilaoDaoTest {
 	
 	@Test
 	public void deveContarLeiloesNaoEncerrados() {
-		// Criamos um usuário
 		Usuario murilo = new Usuario("Murilo", "murilo@cassio.com.br");
 		
 		// Criamos os dois leiloes
@@ -57,6 +58,42 @@ public class LeilaoDaoTest {
 		long total = leilaoDao.total();
 		
 		assertEquals(1L, total);
+	}
+	
+	@Test
+	public void deveRetornarCasoNaoHajaLeilaoNaoEncerrado() {
+		Usuario murilo = new Usuario("Murilo", "murilo@cassio.com.br");
+		
+		Leilao encerrado = new Leilao("Geladeira", 1500.0, murilo, false);
+		Leilao tambemEncerrado = new Leilao("PS4", 1500.0, murilo, false);
+		
+		encerrado.encerra();
+		tambemEncerrado.encerra();
+		
+		usuarioDao.salvar(murilo);
+		leilaoDao.salvar(tambemEncerrado);
+		leilaoDao.salvar(encerrado);
+		
+		long total = leilaoDao.total();
+		
+		assertEquals(0L, total);
+	}
+	
+	@Test
+	public void deveRetornarLeiloesDeProdutosNovos() {
+		Usuario murilo = new Usuario("Murilo", "murilo@cassio.com.br");
+		
+		Leilao produtoNovo = new Leilao("Geladeira", 1500.0, murilo, false);
+		Leilao produtoUsado = new Leilao("PS4", 1500.0, murilo, true);
+		
+		usuarioDao.salvar(murilo);
+		leilaoDao.salvar(produtoNovo);
+		leilaoDao.salvar(produtoUsado);
+		
+		List<Leilao> novos = leilaoDao.novos();
+		
+		assertEquals(1, novos.size());
+		assertEquals("Geladeira", novos.get(0).getNome());
 	}
 
 }
