de5b89a448da3c799cd7df492f2b0447d87d6f01
Cassio Murilo <cmurilo.san@gmail.com>
2020-07-30 15:24:20 -0300

    Praticando com consultas mais complicadas

--- test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
+++ test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertNull;
 
 import org.hibernate.Session;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import br.com.caelum.pm73.dominio.Usuario;
